eaf31b9300b5662f81e5d3a1a8a0967eac943512
unknown <valy_empire@yahoo.com>
2024-03-26 22:37:48 +0200

    Created Experience component

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -1,5 +1,52 @@
+/* eslint-disable react/no-unescaped-entities */
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineContent from "@mui/lab/TimelineContent";
+import ExperienceCard from "../../components/Cards/ExperienceCard";
+import { experiences } from "../../utils/experiences";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Description,
+  TimelineSection,
+} from "./ExperienceStyles";
+
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <Container id="experience">
+      <Wrapper>
+        <Title>Experience</Title>
+        <Description>
+          During my journey as a Front End Developer, I've been fortunate to
+          collaborate closely with seasoned professionals, benefiting greatly
+          from their guidance and constructive feedback, which have been
+          instrumental in honing my skills and expanding my competencies.
+          Furthermore, I've faced a variety of technical challenges and
+          complexities, each presenting a unique opportunity for problem-solving
+          and growth. Embracing these challenges, I've seized every chance to
+          apply my knowledge in practical scenarios, fostering a deeper
+          understanding of software development principles and refining my
+          troubleshooting abilities. In essence, my experience as a Front End
+          Developer has been characterized by a dynamic interplay of learning,
+          collaboration, and overcoming obstacles, laying a solid foundation for
+          my continued growth and success in the ever-evolving landscape of Web
+          Development.
+        </Description>
+        <TimelineSection>
+          <Timeline>
+            {experiences.map((experience, index) => (
+              <TimelineItem key={index}>
+                <TimelineContent sx={{ py: "12px", px: 2 }}>
+                  <ExperienceCard experience={experience} />
+                </TimelineContent>
+              </TimelineItem>
+            ))}
+          </Timeline>
+        </TimelineSection>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Experience;
